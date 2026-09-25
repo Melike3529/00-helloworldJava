5fad116740f986cc39dc6d678c43e1851c8a99cb
makan <makan@350028R-F115X15.lise.kreizenn.bzh>
2026-09-25 11:19:31 +0200

    Exo5

--- src/pkg00/helloworld/Helloworld.java
+++ src/pkg00/helloworld/Helloworld.java
@@ -3,7 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package pkg00.helloworld;
-
+//Je vais utiliser la lib scanner
+import java.util.Scanner;
 /**
  *
  * @author makan
@@ -16,8 +17,49 @@ public class Helloworld {
     public static void main(String[] args) {
         
         //Exo2 Affichge d'un message
+        /*
         System.out.println("Affichage \nligne1 ");
-        System.out.println("Affichage ligne2 ");
+        System.out.println("Affichage ligne2 ");*/
+        
+        //Exo3
+        /*Scanner sc = new Scanner(System.in);
+        int monAge = 0;
+        System.out.print("Quel est votre age ? : ");
+        monAge = sc.nextInt();
+        System.out.println("Vous avez " + monAge + " an(s)");
+        */
+        
+        //Exo4 Permuter var
+        /*Scanner sc = new Scanner(System.in);
+        int x=0;
+        int y=0;
+        int z=0;
+        System.out.print("X : ");
+        x = sc.nextInt();
+        System.out.print("y : ");
+        y = sc.nextInt();
+        System.out.println("x : "+x+" y : " + y);
+        z = x;
+        x = y;
+        y = z;
+        System.out.println("x : "+x+" y : " +y);
+        */
+          
+        //Exo5 Operation arithmetique
+       /*Scanner sc = new Scanner(System.in);
+        int a = 0;
+        int b = 0;
+        int c = 0;
+        System.out.print("a : ");  
+        a = sc.nextInt();
+        System.out.print("b : ");
+        b = sc.nextInt();
+        c = (a+b)*2;
+        System.out.println("c : (a+b)*2 = " + c);
+        */
+        
+     
+        
     }
-    
+   
 }
